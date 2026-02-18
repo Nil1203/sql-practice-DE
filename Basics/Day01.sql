450e7200bafaea5b23f08f7243f07cb35799615e
nilesh desai <120327863+Nil1203@users.noreply.github.com>
2026-02-18 11:24:23 +0530

    Day 1 practice questions
    Added SQL queries to find products that are low fat and recyclable, and to retrieve customer names based on referral conditions.

--- Basics/Day01.sql
+++ Basics/Day01.sql
@@ -1,1 +1,67 @@
+Write a solution to find the ids of products that are both low fat and recyclable.
 
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+
+
+Answer: 
+SELECT product_id FROM Products
+WHERE
+low_fats = 'Y'
+and
+recyclable = 'Y';
+
+
+
+
+Example 2:
+
+Find the names of the customer that are either:
+
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+
+
+
+Answer: 
+
+SELECT name FROM Customer
+WHERE referee_id!=2
+OR referee_id IS NULL;
